769d22cccee3e1223e3012becfe5d8f7df1c36ed
roycetech <royce.com@gmail.com>
2019-07-25 11:40:49 +0800

    Improved test

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -1,19 +1,34 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
 class SolutionTest {
+  @ParameterizedTest
+  @MethodSource("data")
+  void solution(final int[] input, final int expected) {
+    final Solution sut = new Solution();
+    final int actual = sut.solution(input);
+    assertEquals(expected, actual, "output is not as expected");
+  }
 
-    @org.junit.jupiter.api.BeforeEach
-    void setUp() {
+  @SuppressWarnings("PMD.UnusedPrivateMethod")
+  /**
+   * @return
+   */
+  private static Stream<Arguments> data() {
+    final Object[][] expectedActual = new Object[][] {new Object[] {new int[] {}, 1}};
+
+    final List<Arguments> allArguments = new ArrayList<>();
+    for (final Object[] next : expectedActual) {
+      allArguments.add(Arguments.of(next[0], next[1]));
     }
 
-    @org.junit.jupiter.api.AfterEach
-    void tearDown() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void solution() {
-        final Solution sut = new Solution();
-        final int actual = sut.solution(null );
-        assertEquals(-1, actual, "output is not as expected");
-    }  
+    return Stream.of(allArguments.toArray(new Arguments[0]));
+  }
 }
